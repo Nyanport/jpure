28bce076ee4153adb44f4a0737802403a34893ac
Buggem <66670928+Buggem@users.noreply.github.com>
2022-07-07 15:09:45 +1000

    add binaryGarbage feature

--- src/jpure.js
+++ src/jpure.js
@@ -1,12 +1,31 @@
 var jpure = {
   "version": "v0.1",
+  docs: {
+    'chapterI': "\"minimise\" and \"maximise\" explained.\nMinimise and maximise both originate from StackOverflow,\nhttps:\/\/stackoverflow.com\/a\/56867045",
+    'readdoc': function(doc) { console.log(doc); }
+  },
   "minimise": function(binary) {
     return String.fromCharCode(
       ...binary.split(' ').map(bin => parseInt(bin, 2))
     );
   },
-  "binaryGarbage": function() {
-    
+  "maximise": function(string) {
+    // Minimise and maximise functions explained is on line 5 and so on.
+    return str.split('') // split in single chars
+      .map(c => c.charCodeAt(0) // get the UTF-16 code (10 base)
+                 .toString(2)) // transform it back to a string (2 base)
+      .join(' ') // make single string from array
+  },
+
+  "binaryGarbage": function(garbageLen) {
+    var randNum = 8 + Math.floor(Math.random() * (8+1));
+    str binaryGarbage = "";
+    for(int i = 0; i < garbageLen; i++){
+      for(int i = 0; i < randNum; i++) {
+        binaryGarbage += Math.floor(Math.random() * (1+1)).toString();
+      }
+      binaryGarbage += " ";
+    }
   }
 };
 window.jpure = jpure; // give all access to variable
